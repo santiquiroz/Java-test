e55d6eb7172defc63367cdea2efed4db63deea45
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-19 17:57:23 -0500

    operadores unarios en java

--- Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
+++ Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
@@ -10,14 +10,40 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        int a=3, b=2;
-        int c = a + 5 - b;
-        System.out.println(c);
-        
-        a += 1; //a = a + 1
-        System.out.printl(a);
-        
-        a -= 2;
+        var a = 3;
+        var b = -a;
         System.out.println(a);
+        System.out.println(b);
+        
+        var c = true;
+        var d = !c;
+        System.out.println(c);
+        System.out.println(d);
+        
+        //incremento
+        //1.preincremento (simbolo antes de la variable)
+        var e = 3;
+        var f = ++e;
+        System.out.println(e);
+        System.out.println(f);
+        //2.postincremento (simbolo despues de la variable)
+        var g = 5;
+        var h = g++;
+        System.out.println(g);
+        System.out.println(h);
+        
+        //decremento
+        //1.predecremento
+        var i = 2;
+        var j = --i;
+        System.out.println(i);
+        System.out.println(j);
+        
+        //2.postdecremento
+        var k = 4;
+        var l = k--;
+        System.out.println(k);
+        System.out.println(l);
+        
     }
 }
