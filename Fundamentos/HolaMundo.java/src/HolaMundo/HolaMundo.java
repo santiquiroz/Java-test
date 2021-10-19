3e90e1ee985932078a7e9d1d4fee293181d46cfe
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-19 10:45:23 -0500

    caracteres especiales

--- Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
+++ Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
@@ -8,18 +8,12 @@ package HolaMundo;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var usuario = "Juan xd";
-        var titulo = "don Ingeniero";
+        String nombre = "karla";
 
-        var union = titulo + " " + usuario;
-        System.out.println("union = " + union);
-        
-        var i = 3;
-        var j = 4;
-        
-        System.out.println(i + j);//se realiza suma de numeros
-        System.out.println(i + j +usuario);//Evaluacion de izquierda a derecha, hace suma antes
-        System.out.println(usuario + i + j);//contexto cadena
-        System.out.println(usuario + (i + j));//parentesis cambian orden
+        System.out.println("Nueva linea: \n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \b\b" + nombre);
+        System.out.println("Comilla simple \'" + nombre + "\'");
+        System.out.println("Comilla doble: \"" + nombre + "\"");
     }
 }
