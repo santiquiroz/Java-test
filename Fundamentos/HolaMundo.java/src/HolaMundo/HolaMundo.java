0046ceb1ba3f1d54b05f6c02525f0c7854f0ec1c
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-19 11:10:34 -0500

    Tipos numericos enteros

--- Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
+++ Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
@@ -11,12 +11,27 @@ public class HolaMundo {
 
     public static void main(String args[]) {
         /*
-            tipos primitivos enteros: byte, int, short, long
+            tipos primitivos enteros: byte, short, int, long
         */
         
         byte numeroByte = (byte)129;
         System.out.println("numeroByte = " + numeroByte);
         System.out.println("valor minimo byte: " + Byte.MIN_VALUE);
         System.out.println("valor maximo byte: " + Byte.MAX_VALUE);
+        
+        short numeroShort = (short)32768;
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("valor minimo short: " + Short.MIN_VALUE);
+        System.out.println("valor maximo short: " + Short.MAX_VALUE);
+        
+        int numeroInt = (int)2147483648L;
+        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("valor minimo int: " + Integer.MIN_VALUE);
+        System.out.println("valor maximo int: " + Integer.MAX_VALUE);
+        
+        long numeroLong = (long)9223372036854775808F;
+        System.out.println("numeroLong = " + numeroLong);
+        System.out.println("valor minimo long: " + Long.MIN_VALUE);
+        System.out.println("valor maximo long: " + Long.MAX_VALUE);
     }
 }
