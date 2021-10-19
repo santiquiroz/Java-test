e461301fa9f14a0323a313bdc56e73be9a66b5b9
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-18 21:25:20 -0500

    Hola mundo

---
+++ Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
@@ -0,0 +1,12 @@
+//Mi clase en Java
+package HolaMundo;
+
+/**
+ *
+ * @author santi
+ */
+public class HolaMundo {
+    public static void main(String args[]){
+        System.out.println("Hola mundo desde Java ");
+    }
+}
