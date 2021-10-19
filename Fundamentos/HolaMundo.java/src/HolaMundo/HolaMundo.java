65340147d5b2465810199a05c1fe5332b38d868b
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-19 11:18:39 -0500

    tipos primitivos flotantes

--- Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
+++ Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
@@ -11,27 +11,17 @@ public class HolaMundo {
 
     public static void main(String args[]) {
         /*
-            tipos primitivos enteros: byte, short, int, long
+            tipos primitivos flotante: float y double
         */
         
-        byte numeroByte = (byte)129;
-        System.out.println("numeroByte = " + numeroByte);
-        System.out.println("valor minimo byte: " + Byte.MIN_VALUE);
-        System.out.println("valor maximo byte: " + Byte.MAX_VALUE);
+        float numeroFloat = (float)3.4028235E38;
+        System.out.println("numeroFloat = " + numeroFloat);
+        System.out.println("Valor minimo tipo float: " + Float.MIN_VALUE);
+        System.out.println("Valor minimo tipo float: " + Float.MAX_VALUE);
         
-        short numeroShort = (short)32768;
-        System.out.println("numeroShort = " + numeroShort);
-        System.out.println("valor minimo short: " + Short.MIN_VALUE);
-        System.out.println("valor maximo short: " + Short.MAX_VALUE);
-        
-        int numeroInt = (int)2147483648L;
-        System.out.println("numeroInt = " + numeroInt);
-        System.out.println("valor minimo int: " + Integer.MIN_VALUE);
-        System.out.println("valor maximo int: " + Integer.MAX_VALUE);
-        
-        long numeroLong = (long)9223372036854775808F;
-        System.out.println("numeroLong = " + numeroLong);
-        System.out.println("valor minimo long: " + Long.MIN_VALUE);
-        System.out.println("valor maximo long: " + Long.MAX_VALUE);
+        double numeroDouble = 1.7976931348623157E308;
+        System.out.println("numeroDouble = " + numeroDouble);
+        System.out.println("Valor minimo tipo double: " + Double.MIN_VALUE);
+        System.out.println("Valor minimo tipo double: " + Double.MAX_VALUE);
     }
 }
