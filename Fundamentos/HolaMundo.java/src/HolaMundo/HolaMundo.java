5ca53b3bcf60248306703524fce124085c0664e2
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-18 22:04:58 -0500

    variables

--- Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
+++ Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
@@ -7,6 +7,7 @@ package HolaMundo;
  */
 public class HolaMundo {
     public static void main(String args[]){
-        System.out.println("Hola mundo desde Java ");
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
     }
 }
