6baa3bf8a0439d6968dd3d7da2c9d0db9122d5a2
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-18 22:12:13 -0500

    Tipos enteros y String

--- Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
+++ Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
@@ -7,7 +7,18 @@ package HolaMundo;
  */
 public class HolaMundo {
     public static void main(String args[]){
+        //Definimos la variable
         int miVariableEntera = 10;
         System.out.println(miVariableEntera);
+        
+        //Modificamos el valor de la varaible
+        miVariableEntera = 5;        
+        System.out.println(miVariableEntera);
+        
+        String miVariableCadena = "Salu2";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena = "aGod";
+        System.out.println(miVariableCadena);
     }
 }
