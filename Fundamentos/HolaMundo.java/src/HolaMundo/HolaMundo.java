d5bdbdae78366b8570afbd1614139e1f16662462
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-20 12:47:00 -0500

    Ciclos en Java

--- Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
+++ Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
@@ -10,29 +10,10 @@ import java.util.Scanner;
 public class HolaMundo {
     
     public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Proporciona un valor entre 0 y 10: ");
-        var numero = Integer.parseInt(scanner.nextLine());
-        var nota = "Nota desconocida";
-        if (0 >= numero && numero < 6) {
-            nota = "F";
+        var contador = 0;
+        while( contador < 3){
+            System.out.println("contador = " + contador);
+            contador++;
         }
-        else{
-            switch(numero){
-                case 6:
-                    nota = "D";
-                    break;
-                case 7:
-                    nota = "C";
-                    break;
-                case 8:
-                    nota = "B";
-                    break;
-                case 9: case 10:
-                    nota = "A";
-                    break;
-            }
-        }
-        System.out.println("nota = " + nota);
     }
 }
