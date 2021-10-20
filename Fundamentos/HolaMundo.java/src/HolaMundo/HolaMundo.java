5ef4055ff6785511d94511384b2dbdf97270408e
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-20 12:49:33 -0500

    ciclo do while

--- Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
+++ Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
@@ -10,10 +10,16 @@ import java.util.Scanner;
 public class HolaMundo {
     
     public static void main(String args[]) {
+//        var contador = 0;
+//        while( contador < 3){
+//            System.out.println("contador = " + contador);
+//            contador++;
+//        }
         var contador = 0;
-        while( contador < 3){
+        do {
             System.out.println("contador = " + contador);
             contador++;
-        }
+        } while (contador < 0);
+        
     }
 }
