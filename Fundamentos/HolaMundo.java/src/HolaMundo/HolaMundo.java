680bfdb9738fe6791c0225b082386ee0e3f1da9b
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-20 12:54:53 -0500

    break y continue en java

--- Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
+++ Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
@@ -10,21 +10,31 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        
         for (var contador = 0; contador < 10; contador++) {
+            if (contador % 2 != 0) {
+                continue;
+            }
             System.out.println("contador = " + contador);
         }
+        
+//        for (var contador = 0; contador < 10; contador++) {
+//            if (contador % 2 == 0) {
+//                System.out.println("contador = " + contador);
+//                break;
+//            }
+//            System.out.println("contador = " + contador);
+//        }
         
 //        var contador = 0;
 //        while( contador < 3){
 //            System.out.println("contador = " + contador);
 //            contador++;
 //        }
-        var contador = 0;
-        do {
-            System.out.println("contador = " + contador);
-            contador++;
-        } while (contador < 0);
+//        var contador = 0;
+//        do {
+//            System.out.println("contador = " + contador);
+//            contador++;
+//        } while (contador < 0);
 
     }
 }
