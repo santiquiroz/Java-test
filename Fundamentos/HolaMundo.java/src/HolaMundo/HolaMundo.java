f00f982bc14a155102a1bf5319c6319badb53511
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-20 12:56:21 -0500

    Etiquetas en Java

--- Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
+++ Fundamentos/HolaMundo.java/src/HolaMundo/HolaMundo.java
@@ -10,9 +10,11 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
+        
+        inicio:
         for (var contador = 0; contador < 10; contador++) {
             if (contador % 2 != 0) {
-                continue;
+                continue inicio;
             }
             System.out.println("contador = " + contador);
         }
