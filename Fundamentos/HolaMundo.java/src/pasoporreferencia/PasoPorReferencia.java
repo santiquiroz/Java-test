7534845f52ce423d448b0f947a650b2e22b29702
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-20 21:14:18 -0500

    return y null

--- Fundamentos/HolaMundo.java/src/pasoporreferencia/PasoPorReferencia.java
+++ Fundamentos/HolaMundo.java/src/pasoporreferencia/PasoPorReferencia.java
@@ -9,14 +9,20 @@ import clases.Persona;
  */
 public class PasoPorReferencia {
     public static void main(String[] args) {
-        Persona persona1 = new Persona();
-        persona1.nombre = "Kuan xd xd";
-        System.out.println("persona1 nombre = " + persona1.nombre);
-        cambioValor(persona1);
-        System.out.println("persona1 cambio nombre = " + persona1.nombre);
+        //Persona persona1 = new Persona();
+        Persona persona1 = null;
+//        persona1.nombre = "Kuan xd xd";
+//        System.out.println("persona1 nombre = " + persona1.nombre);
+        persona1 = cambioValor(persona1);
+//        System.out.println("persona1 cambio nombre = " + persona1.nombre);
     }
     
-    public static void cambioValor(Persona persona){
+    public static Persona cambioValor(Persona persona){
+        if (persona == null) {
+            System.out.println("valor de persona invalido: null");
+            return null;
+        }
         persona.nombre = "Karla";
+        return persona;
     }
 }
