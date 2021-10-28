da70a7cc41088676dae623441e5cffc1e7a5ca3b
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-28 10:34:45 -0500

    toString

--- Fundamentos/Encapsulamiento/src/test/PersonaPrueba.java
+++ Fundamentos/Encapsulamiento/src/test/PersonaPrueba.java
@@ -9,7 +9,9 @@ public class PersonaPrueba {
     public static void main(String[] args) {
         Persona persona1 = new Persona("Juan xds", 5000.00, false);
         persona1.setNombre("JUan carols");
-        System.out.println("Persona 1 nombre: "+persona1.getNombre());
-        System.out.println("persona1 sueldo= " + persona1.getSueldo());
+//        System.out.println("Persona 1 nombre: "+persona1.getNombre());
+//        System.out.println("persona1 sueldo= " + persona1.getSueldo());
+//        System.out.println("persona1 eliminado= " + persona1.isEliminado());
+        System.out.println("persona1: " + persona1);
     }
 }
