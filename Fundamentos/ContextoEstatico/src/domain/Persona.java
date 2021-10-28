97a74d7e096f571467b95ab0142a965d5b7dda44
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-28 14:05:48 -0500

    ejercicio contexto estatico

--- Fundamentos/ContextoEstatico/src/domain/Persona.java
+++ Fundamentos/ContextoEstatico/src/domain/Persona.java
@@ -16,4 +16,35 @@ public class Persona {
         
         this.idPersona = Persona.contadorPersonas;
     }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public static int getContadorPersonas() {
+        return contadorPersonas;
+    }
+
+    public static void setContadorPersonas(int contadorPersonas) {
+        Persona.contadorPersonas = contadorPersonas;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + ", nombre=" + nombre + '}';
+    }
+    
+    
 }
