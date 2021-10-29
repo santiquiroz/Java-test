eecf02711ac3d3ef1cbdc20e5493a9ebb975ddf5
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-29 10:11:36 -0500

    paquetes en Java

---
+++ Fundamentos/Pquetes/src/mx/com/globalmentoring/Utileria.java
@@ -0,0 +1,11 @@
+package mx.com.globalmentoring;
+
+/**
+ *
+ * @author santi
+ */
+public class Utileria {
+    public static void imprimir(String s){
+        System.out.println("s = " + s);
+    }
+}
---
+++ Fundamentos/Pquetes/src/test/TestUtileria.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ *
+ * @author santi
+ */
+
+//import mx.com.globalmentoring.Utileria;
+import static mx.com.globalmentoring.Utileria.imprimir;
+
+public class TestUtileria {
+    public static void main(String[] args) {
+//        imprimir("Saludos");
+        mx.com.globalmentoring.Utileria.imprimir("adios");
+    }
+}
