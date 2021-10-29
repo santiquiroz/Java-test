6500fb80976bd67d752c4c2328b2e50aa76357e2
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-29 10:04:00 -0500

    sobrecarga de metodos

---
+++ Fundamentos/SobrecargaMetodos/src/operaciones/Operaciones.java
@@ -0,0 +1,15 @@
+package operaciones;
+
+/**
+ *
+ * @author santi
+ */
+public class Operaciones {
+    public static int sumar(int a, int b){
+        return a + b;
+    }
+    
+    public static double sumar(double a, double b){
+        return a + b;
+    }
+}
---
+++ Fundamentos/SobrecargaMetodos/src/test/TestOperaciones.java
@@ -0,0 +1,20 @@
+package test;
+
+import operaciones.Operaciones;
+
+/**
+ *
+ * @author santi
+ */
+public class TestOperaciones {
+    public static void main(String[] args) {
+        var resultado = Operaciones.sumar(5, 3);
+        System.out.println("resultado = " + resultado);
+        
+        var resultado2 = Operaciones.sumar(2.0, 3);
+        System.out.println("resultado2 = " + resultado2);
+        
+        var resultado3 = Operaciones.sumar(3, 5L);
+        System.out.println("resultado3 = " + resultado3);
+    }
+}
