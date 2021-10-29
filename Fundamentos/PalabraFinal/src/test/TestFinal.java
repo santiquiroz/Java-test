60ea05cea73d7a8606767ff47ece85688a4ea880
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-10-29 10:52:13 -0500

    final con Objetos

--- Fundamentos/PalabraFinal/src/test/TestFinal.java
+++ Fundamentos/PalabraFinal/src/test/TestFinal.java
@@ -13,5 +13,12 @@ public class TestFinal {
         
 //        Persona.MI_CONSTANTE = 5;
         System.out.println("miConstante = " +  Persona.MI_CONSTANTE  );
+        
+        final Persona persona1 = new Persona();
+        //persona1 = new Persona();
+        persona1.setNombre("Juan");
+        System.out.println("persona1 nombre = " + persona1.getNombre());
+        persona1.setNombre("Camilo");
+        System.out.println("persona1 nombre = " + persona1.getNombre());
     }
 }
