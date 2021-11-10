2a7d6c80f8d718ab3c6f7021531ea95f907506b2
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-11-10 18:50:25 -0500

    manejo de interfaces

---
+++ Programacion/Interfaces/src/accesodatos/IAccesoDatos.java
@@ -0,0 +1,17 @@
+package accesodatos;
+
+/**
+ *
+ * @author santi
+ */
+public interface IAccesoDatos {
+    int MAX_REGISTRO = 10;
+    
+    void insertar();
+    
+    void listar();
+    
+    void actualizar();
+    
+    void eliminar();
+}
