c5ca23a733ddf98efe2d8eeaf3c4a6bb0ad94743
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-11-10 16:13:49 -0500

    polimorfismo

--- Programacion/Sobrescritura/src/test/TestSobrescritura.java
+++ Programacion/Sobrescritura/src/test/TestSobrescritura.java
@@ -1,5 +1,6 @@
 package test;
 
+import domain.Empleado;
 import domain.Gerente;
 
 /**
@@ -8,7 +9,18 @@ import domain.Gerente;
  */
 public class TestSobrescritura {
     public static void main(String[] args) {
-        Gerente gerente1 = new Gerente("Juan", 5000, "Sistemas");
-        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+        Empleado  empeado = new Empleado("Juan", 5000);
+//        System.out.println("empeado = " + empeado.obtenerDetalles());
+        imprimir(empeado);
+        
+        Gerente gerente = new Gerente("Karla", 1000, "Contabilidad");
+        
+        imprimir(gerente);
+        
+//        System.out.println("gerente = " + gerente.obtenerDetalles());
+    }
+    
+    public static void imprimir(Empleado empleado){
+        System.out.println("empleado = " + empleado.obtenerDetalles());
     }
 }
