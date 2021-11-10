115caee78247dd6a00c79811b881c8026d9766db
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-11-10 17:13:38 -0500

    conversion de objetos

---
+++ Programacion/ConversionObjetos/src/test/TestConversionObjetos.java
@@ -0,0 +1,28 @@
+package test;
+
+import domain.Empleado;
+import domain.Escritor;
+import domain.TipoEscritura;
+
+/**
+ *
+ * @author santi
+ */
+public class TestConversionObjetos {
+    public static void main(String[] args) {
+        Empleado empleado;
+        empleado = new Escritor("Juan", 5000, TipoEscritura.CLASICO);
+//        System.out.println("empleado = " + empleado);
+        
+        System.out.println(empleado.obtenerDetalles());
+        
+        
+        //downcasting
+        //((Escritor) empleado).getTipoEscritura();
+        Escritor escritor = (Escritor) empleado;
+        escritor.getTipoEscritura();
+        
+        Empleado empleado2 = escritor;
+        System.out.println("empleado2 = " + empleado2.obtenerDetalles());
+    }
+}
